ef8b0a3260b37393a471edc31b03bf01918aa547
BrunoPeregrinoIFCE <peregrino.bruno62@aluno.ifce.edu.br>
2026-09-18 16:11:10 -0300

    questao26.c

--- Lista 1/questao26.c
+++ Lista 1/questao26.c
@@ -2,13 +2,12 @@
 #include <math.h>
 
 int main() {
-    float n1, n2, n3;
-    float media_aritmetica, media_geometrica;
+    float n1, n2, n3, media_aritmetica, media_geometrica;
 
     printf("Digite três números: ");
     scanf("%f %f %f", &n1, &n2, &n3);
 
-    media_aritmetica = (n1 + n2 + n3) / 3;
+    media_aritmetica = (n1 + n2 + n3) / 3.0;
     media_geometrica = pow(n1 * n2 * n3, 1.0 / 3.0);
 
     printf("Media aritmetica: %.2f\n", media_aritmetica);
